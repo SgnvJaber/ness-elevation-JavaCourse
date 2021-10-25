cf4ac8848c68d725054d707c932061055999c788
SgnvJaber <sgnvunleashed5@gmail.com>
2021-10-25 19:33:17 +0300

    solved Statements06

---
+++ src/lesson01/Statements06.java
@@ -0,0 +1,22 @@
+package lesson01;
+
+public class Statements06 {
+    public static void main(String[] args) {
+        String job = "Teacher";
+        switch (job) {
+            case "Teacher":
+                System.out.println(5000);
+                break;
+            case "Bank Teller":
+                System.out.println(10000);
+                break;
+            case "QA":
+                System.out.println(15000);
+                break;
+            case "Average Salary":
+                System.out.println(9100);
+                break;
+
+        }
+    }
+}
